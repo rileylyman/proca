1fc3c6dafec2c684b68d183c69f1393e4563fa2a
Riley Lyman <rileylyman@gmail.com>
2024-07-15 20:44:58 -0700

    commit

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-clang main.c -o main.exe -L.\raylib\lib -lmsvcrt -lraylib -lOpenGL32 -lGdi32 -lWinMM -lkernel32 -lshell32 -lUser32 -Xlinker /NODEFAULTLIB:libcmt
+clang main.c -o main.exe -g -O0 -L.\raylib\lib -lmsvcrt -lraylib -lOpenGL32 -lGdi32 -lWinMM -lkernel32 -lshell32 -lUser32 -Xlinker /NODEFAULTLIB:libcmt
--- main.c
+++ main.c
@@ -1,88 +1,93 @@
 // https://stackoverflow.com/questions/72208683/what-standard-libraries-do-i-need-to-link-to-use-raylib-with-windows-and-clang
 #include <math.h>
 #include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
 
 #include "raylib/include/raylib.h"
 #include "raylib/include/raymath.h"
 
-#define BLACK                                                                  \
+#define ONYX                                                                  \
   (Color) { 50, 55, 59, 255 }
 #define GREY                                                                   \
   (Color) { 74, 88, 89, 255 }
-#define PINK                                                                   \
+#define SAKURA                                                                   \
   (Color) { 244, 214, 204, 255 }
-#define YELLOW                                                                 \
+#define POPPY                                                                 \
   (Color) { 244, 184, 96, 255 }
-#define RED                                                                    \
+#define CHERRY                                                                    \
   (Color) { 200, 62, 77, 255 }
 
 const int LINE_THICKNESS = 1;
+const float SPEED_PPS = 300;
 
 typedef struct Joint Joint;
 typedef struct Joint {
   Vector2 pos;
   float radius;
-  Joint *constraint;
 } Joint;
 
-void UpdateJointPos(Joint *joint) {
+void UpdateJointPos(Joint *joint, Joint* constraint) {
+  assert(joint && constraint);
   Vector2 pos = joint->pos;
 
-  if (joint->constraint) {
-    float x = joint->pos.x - joint->constraint->pos.x;
-    float y = -(joint->pos.y - joint->constraint->pos.y);
-    float r = sqrt(x * x + y * y);
-    if (true || r > joint->constraint->radius) {
-      float theta = atan(y / x);
-      if (x < 0) {
-        theta += 3.14;
-      }
-      // if (theta < 0) {
-      //   theta += 6.28;
-      // }
-      // printf("%f, %f\n", r, theta / 6.28 * 360);
+  float x = joint->pos.x - constraint->pos.x;
+  float y = -(joint->pos.y - constraint->pos.y);
+  float r = sqrt(x * x + y * y);
+  if (true || r > constraint->radius) {
+    float theta = atan(y / x);
+    if (x < 0) {
+      theta += 3.14;
+    }
+    // if (theta < 0) {
+    //   theta += 6.28;
+    // }
+    // printf("%f, %f\n", r, theta / 6.28 * 360);
 
-      pos.x = joint->constraint->pos.x + joint->constraint->radius * cos(theta);
-      pos.y = joint->constraint->pos.y - joint->constraint->radius * sin(theta);
-      joint->pos = pos;
+    pos.x = constraint->pos.x + constraint->radius * cos(theta);
+    pos.y = constraint->pos.y - constraint->radius * sin(theta);
+    joint->pos = pos;
+  }
+}
+
+void DrawJoint(Joint *joint, Joint* constraint) {
+  assert(joint);
+  DrawRing(joint->pos, joint->radius - LINE_THICKNESS, joint->radius, 0, 360,
+           360, SAKURA);
+
+  const float inner_radius = 10;
+  DrawRing(joint->pos, inner_radius - LINE_THICKNESS, inner_radius, 0, 360, 360,
+           CHERRY);
+
+  if (constraint) {
+    DrawLineV(constraint->pos, joint->pos, POPPY);
+  }
+}
+
+typedef struct Spine {
+  Joint* joints;
+  int num_joints;
+} Spine;
+
+
+void DrawBody(Spine *spine, bool draw_joints) {
+  if (draw_joints) {
+    DrawJoint(&spine->joints[0], NULL);
+    for (int i = 1; i < spine->num_joints; i++) {
+      DrawJoint(&spine->joints[i], &spine->joints[i - 1]);
     }
   }
 }
 
-void DrawJoint(Joint *joint) {
-  DrawRing(joint->pos, joint->radius - LINE_THICKNESS, joint->radius, 0, 360,
-           360, PINK);
+void UpdateSpinePos(Spine* spine, Vector2 target_pos) {
+  float delta_time = GetFrameTime();
+  if (Vector2Distance(spine->joints[0].pos, target_pos)) {
+    spine->joints[0].pos = Vector2MoveTowards(spine->joints[0].pos, target_pos, SPEED_PPS * delta_time);
 
-  const float inner_radius = 10;
-  DrawRing(joint->pos, inner_radius - LINE_THICKNESS, inner_radius, 0, 360, 360,
-           RED);
-
-  if (joint->constraint) {
-    DrawLineV(joint->constraint->pos, joint->pos, ORANGE);
   }
-}
-
-void DrawBody(Joint *joint) {
-  if (!joint->constraint) {
-    return;
+  for (int i = 1; i < spine->num_joints; i++) {
+    UpdateJointPos(&spine->joints[i], &spine->joints[i - 1]);
   }
-  const int steps = 50;
-
-  Vector2 dir =
-      Vector2Normalize(Vector2Subtract(joint->constraint->pos, joint->pos));
-  Vector2 cross = {-dir.y, dir.x};
-
-  for (int i = 0; i < steps; i++) {
-    float t = (float)i / (float)steps;
-    Vector2 pos = Vector2Lerp(joint->pos, joint->constraint->pos, t);
-    float radius = Lerp(joint->radius, joint->constraint->radius, t);
-    Vector2 out1 = Vector2Add(pos, Vector2Scale(cross, radius));
-    Vector2 out2 = Vector2Add(pos, Vector2Scale(cross, -radius));
-    DrawPixelV(out1, PINK);
-    DrawPixelV(out2, PINK);
-  }
-
-  DrawBody(joint->constraint);
 }
 
 int main() {
@@ -90,35 +95,30 @@ int main() {
   const int screenHeight = 1080;
 
   Joint joints[] = {
-      {.pos = {300, 300}, .radius = 50.0, .constraint = NULL},
-      {.pos = {200, 100}, .radius = 75.0, .constraint = NULL},
-      {.pos = {200, 500}, .radius = 60.0, .constraint = NULL},
-      {.pos = {200, 500}, .radius = 50.0, .constraint = NULL},
+      {.pos = {300, 300}, .radius = 50.0},
+      {.pos = {200, 100}, .radius = 75.0},
+      {.pos = {200, 500}, .radius = 60.0},
+      {.pos = {200, 500}, .radius = 50.0},
   };
-  joints[1].constraint = &joints[0];
-  joints[2].constraint = &joints[1];
-  joints[3].constraint = &joints[2];
   const int num_joints = sizeof(joints) / sizeof(Joint);
+  Spine spine = {.joints = joints, .num_joints = num_joints};
 
   SetConfigFlags(FLAG_MSAA_4X_HINT);
   InitWindow(screenWidth, screenHeight, "");
 
+  Vector2 target_pos = joints[0].pos;
+
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
+    if (true || IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+      target_pos = GetMousePosition();
+    }
+    UpdateSpinePos(&spine, target_pos);
+
     BeginDrawing();
     ClearBackground(GREY);
 
-    Vector2 mouse_pos = GetMousePosition();
-    joints[0].pos = mouse_pos;
-
-    for (int i = 0; i < num_joints; i++) {
-      UpdateJointPos(&joints[i]);
-      DrawJoint(&joints[i]);
-    }
-    DrawJoint(&joints[0]);
-    DrawJoint(&joints[num_joints - 1]);
-
-    DrawBody(&joints[num_joints - 1]);
+    DrawBody(&spine, true);
 
     EndDrawing();
   }
